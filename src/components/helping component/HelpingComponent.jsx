3e4689d566595e4276e4db92803107e57c6e9bb3
A-DILEEP <dileepalla2003@gmail.com>
2024-06-30 21:24:04 +0530

    completed helping section with using helping component.js

--- src/components/helping component/HelpingComponent.jsx
+++ src/components/helping component/HelpingComponent.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
-const HelpingComponent = () => {
+import "./helpingComponent.css";
+const HelpingComponent = ({img,count,text}) => {
   return (
-    <div>
-      
+    <div className="helping-component-container">
+      <div className="helping-component-container-left">
+        <img src={img} alt="people" />
+      </div>
+      <div className="helping-component-container-right">
+        <div className="helping-component-container-count">
+          {count}
+        </div>
+        <div className="helping-component-container-text">
+          {text}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HelpingComponent
--- src/containers/helping/Helping.jsx
+++ src/containers/helping/Helping.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import './helping.css'
-import HelpingComponent from '../../components/helping component/HelpingComponent'
+import React from "react";
+import "./helping.css";
+import HelpingComponent from "../../components/helping component/HelpingComponent";
+import h1 from "../../assets/h1.png";
+import h2 from "../../assets/h2.png";
+import h3 from "../../assets/h3.png";
+import h4 from "../../assets/h4.png";
 const Helping = () => {
   return (
     <div className="helping">
@@ -13,13 +17,13 @@ const Helping = () => {
         </div>
       </div>
       <div className="helping-right">
-        <HelpingComponent />
-        <HelpingComponent />
-        <HelpingComponent />
-        <HelpingComponent />
+        <HelpingComponent img={h1} text="Members" count="2,245,341" />
+        <HelpingComponent img={h2} text="Clubs" count="46,328" />
+        <HelpingComponent img={h3} text="Event Booking" count="828,867" />
+        <HelpingComponent img={h4} text="Payments" count="1,926,436" />
       </div>
     </div>
   );
-}
+};
 
 export default Helping;
