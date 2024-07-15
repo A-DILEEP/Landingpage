2f7eeb60356e6c0c99c4541c11dad35c2c79ef17
A-DILEEP <dileepalla2003@gmail.com>
2024-07-15 18:09:29 +0530

    completed half footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 import "./footer.css";
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-heading">
+          <p>
+            Pellentesque suscipit <br /> fringilla libero eu.
+          </p>
+        </div>
+        <div className="getADemo">
+          <button>Get a Demo</button>
+        </div>
+      </div>
+      <div className="footer-bottom">
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer
