dbd6d3214adfd08376f5a5b7aa14dc7fe7bff6ce
A-DILEEP <dileepalla2003@gmail.com>
2024-07-15 19:43:39 +0530

    Completed footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React from "react";
 import "./footer.css";
+import { SiInstagram } from "react-icons/si";
+import { FaDribbble } from "react-icons/fa";
+import { SlSocialTwitter } from "react-icons/sl";
+import { AiOutlineYoutube } from "react-icons/ai";
+
 const Footer = () => {
   return (
     <div className="footer">
@@ -14,10 +19,78 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-
+        <div className="footer-bottom-nexcent">
+          <div className="footer-bottom-nexcent-heading">Nexcent</div>
+          <div className="footer-bottom-nexcent-minText">
+            <p>Copyright © 2020 Landify UI Kit.</p>
+            <p>All rights reserved</p>
+          </div>
+          <div className="footer-bottom-nexcent-icons">
+            <button>
+              <SiInstagram />
+            </button>
+            <button>
+              <FaDribbble />
+            </button>
+            <button>
+              <SlSocialTwitter />
+            </button>
+            <button>
+              <AiOutlineYoutube />
+            </button>
+          </div>
+        </div>
+        <div className="footer-bottom-company">
+          <div className="footer-bottom-company-heading">Company</div>
+          <div className="footer-bottom-company-links">
+            <p>
+              <a href="About Us">About Us</a>
+            </p>
+            <p>
+              <a href="Blog">Blog</a>
+            </p>
+            <p>
+              <a href="Contact Us">Contact Us</a>
+            </p>
+            <p>
+              <a href="Pricing">Pricing</a>
+            </p>
+            <p>
+              <a href="Testimonials">Testimonials</a>
+            </p>
+          </div>
+        </div>
+        <div className="footer-bottom-support">
+          <div className="footer-bottom-support-heading">Support</div>
+          <div className="footer-bottom-support-links">
+            <p>
+              <a href="Help center">Help center</a>
+            </p>
+            <p>
+              <a href="Terms of service">Terms of service</a>
+            </p>
+            <p>
+              <a href="Legal">Legal</a>
+            </p>
+            <p>
+              <a href="Privacy policy">Privacy policy</a>
+            </p>
+            <p>
+              <a href="Status">Status</a>
+            </p>
+          </div>
+        </div>
+        <div className="footer-bottom-stayUpTwoDate">
+          <div className="footer-bottom-stayUpTwoDate-heading">
+            Stay up to date
+          </div>
+          <div className="footer-bottom-input">
+            <input type="email" placeholder="Your Email Address" />
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Footer
+export default Footer;
